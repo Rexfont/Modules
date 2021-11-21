b76fb610e488c875b0040e29929616b56b1bb890
aXlireza <alireza.ataee01@gmail.com>
2021-11-21 16:52:21 +0330

    git init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const multer  = require('multer')
+const cors = require('cors')
+app.use(cors())
+const upload = multer({ dest: '../../../uploads/' })
+const svgjson = require('svgjson');
+const root = '/svgjson';
+const PORT = process.env.PORT || 8001;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} : ${req._parsedUrl.path}`);
+  next();
+})
+
+app.get(`${root}/`, (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+
+app.post(`${root}/convert`, upload.single('file'), async (req, res) => {
+  if(req.file) svgjson({data: path.join(__dirname, `./${req.file.path}`), web: false, file: true, remove: true, output: false, filename: 'sasho.svg'})
+      .then(response => res.send(response))
+      .catch(error => {
+        console.log(error.stack?error.stack:error);
+        res.status(400).send(error.stack?error.stack:error)
+      })
+  else svgjson({data: req.body.code, web: false, file: false, remove: false, output: false, filename: 'sasho.svg'})
+      .then(response => res.send(response))
+      .catch(error => {
+        console.log(error.stack?error.stack:error);
+        res.status(400).send(error.stack?error.stack:error)
+      })
+  
+})
+
+
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
