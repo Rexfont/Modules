42a83556ef682fa7de6ceecec34adabc182aa597
aXlireza <alireza.ataee01@gmail.com>
2021-11-25 12:29:47 +0330

    adding other options to html and ability to file download

--- app.js
+++ app.js
@@ -19,18 +19,25 @@ app.get(`${svgjsonRoot}/`, (req, res) => {
 });
 
 app.post(`${svgjsonRoot}/convert`, upload.single('file'), async (req, res) => {
-  if(req.file) svgjson({input: path.join(__dirname, `./${req.file.path}`)})
-      .then(response => res.send(response))
-      .catch(error => {
-        console.log(error.stack?error.stack:error);
-        res.status(400).send(error.stack?error.stack:error)
-      })
-  else svgjson({input: req.body.code})
-      .then(response => res.send(response))
-      .catch(error => {
-        console.log(error.stack?error.stack:error);
-        res.status(400).send(error.stack?error.stack:error)
-      })
+
+  const otherInputs = {};
+  if(req.body.outputFormat) otherInputs['outputFormat'] = req.body.outputFormat;
+  if(req.body.unify) otherInputs['unify'] = req.body.unify;
+
+  if(req.file) 
+    svgjson({input: path.join(__dirname, `./${req.file.path}`), ...otherInputs})
+    .then(response => res.send(response))
+    .catch(error => {
+      console.log(error.stack?error.stack:error);
+      re+s.status(400).send(error.stack?error.stack:error)
+    });
+  else 
+    svgjson({input: req.body.code, ...otherInputs})
+    .then(response => res.send(response))
+    .catch(error => {
+      console.log(error.stack?error.stack:error);
+      res.status(400).send(error.stack?error.stack:error)
+    });
 })
 
 
