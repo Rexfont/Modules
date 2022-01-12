55f14a3efd7a641f27c905fba69724ef212ed5ad
aXlireza <alireza.ataee01@gmail.com>
2022-01-12 15:28:54 +0330

    update

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const multer  = require('multer')
 const cors = require('cors')
 app.use(cors())
 const upload = multer({ dest: '../uploads/' })
-const svgjson = require('svgjson');
+// const svgjson = require('svgjson');
+const svgjson = require('../svgJson/index');
 const svgjsonRoot = '/svgjson';
 const PORT = process.env.PORT || 8001;
 
@@ -13,7 +14,6 @@ app.use((req, res, next) => {
   console.log(`${req.method} : ${req._parsedUrl.path}`);
   next();
 })
-
 app.get(`${svgjsonRoot}/`, (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
@@ -22,20 +22,20 @@ app.post(`${svgjsonRoot}/convert`, upload.single('file'), async (req, res) => {
 
   const otherInputs = {};
   if(req.body.outputFormat) otherInputs['outputFormat'] = req.body.outputFormat;
-  if(req.body.unify) otherInputs['unify'] = req.body.unify;
+  if(req.body.unify) otherInputs['unifySvg'] = req.body.unify.indexOf('true') ? true : false;
 
   if(req.file) 
-    svgjson({input: path.join(__dirname, `./${req.file.path}`), ...otherInputs})
+    svgjson.convert({input: path.join(__dirname, `./${req.file.path}`), ...otherInputs})
     .then(response => res.send(response))
     .catch(error => {
-      console.log(error.stack?error.stack:error);
-      re+s.status(400).send(error.stack?error.stack:error)
+      console.error(error.stack?error.stack:error);
+      res.status(400).send(error.stack?error.stack:error)
     });
   else 
-    svgjson({input: req.body.code, ...otherInputs})
+    svgjson.convert({input: req.body.code, ...otherInputs})
     .then(response => res.send(response))
     .catch(error => {
-      console.log(error.stack?error.stack:error);
+      console.error(error.stack?error.stack:error);
       res.status(400).send(error.stack?error.stack:error)
     });
 })
